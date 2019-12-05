62da4aef059f3d6992028fb3b2f6bd72a3110a94
Sai Gongidi <saigongidi@gmail.com>
2019-12-05 05:06:29 -0500

    route pages

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,20 @@ import express from "express";
 
 export const router = express.Router();
 export const prefix = '/';
+const path = require('path');
 
-
-/**
- * Hello world for the root
- */
-router.get('/', function (req, res) {
-  res.send({
-    status: 'You have reached the root of the API!',
-    repo: 'https://github.com/cgburgess/comp426-backend'
-  })
+router.get('/style', (req, res) => { // style
+  res.sendFile(path.resolve('site/style.css'));
 });
 
+router.get('/', (req, res) => {  // home page
+  res.sendFile(path.resolve('site/index.html'));
+});
+
+router.get('/vlogin', (req, res) => {  // venue login page
+  res.sendFile(path.resolve('site/venue_login.html'));
+});
+
+router.get('/vsignup', (req, res) => {  // venue signup page
+  res.sendFile(path.resolve('site/venue_signup.html'));
+});
